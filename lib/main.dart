0f9900169458e54056ede1b2196027275e4d6fcf
Lokman Hossen <lokmanhossen230@gmail.com>
2024-09-23 11:54:30 +0600

    Update

--- lib/main.dart
+++ lib/main.dart
@@ -10,6 +10,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       title: 'Flutter Groceries',
       theme: ThemeData.dark().copyWith(
         useMaterial3: true,
